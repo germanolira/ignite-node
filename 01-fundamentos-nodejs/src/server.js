a1819a7b000187fd6638135a9b28dfabf0628551
germanolira <dolan.3@hotmail.com>
2023-05-08 05:06:08 -0300

    saving users

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -17,14 +17,24 @@ import http from 'node:http';
 // PATCH => Atualizar uma informação específica de um recurso no back-end
 // DELETE => Deletar uma recurso do back-end
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagens de usuários');
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .JSON.stringify(users)
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@email.com'
+    })
+
     return res.end('Criação de usuários');
   }
 
