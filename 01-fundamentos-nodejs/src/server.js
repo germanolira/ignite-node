1f44cfeee34f62b8341793c48b1fdd85a6557de6
germanolira <dolan.3@hotmail.com>
2023-05-08 04:44:17 -0300

    routes

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,33 @@
 import http from 'node:http';
 
+// - Criar usuários
+// - Listagem usuários
+// - Edição usuários
+// - Remoção usuários
+
+// - HTTP
+//   - Métodos HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar uma recurso do back-end
+// POST => Criar uma recurso no back-end
+// PUT => Atualizar uma recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar uma recurso do back-end
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagens de usuários');
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuários');
+  }
+
   return res.end('Hello World');
 })
 
