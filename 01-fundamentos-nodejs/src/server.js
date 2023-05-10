dbe45967263a552b07b4e49e1fb82a24b28cf995
germanolira <dolan.3@hotmail.com>
2023-05-10 19:33:36 -0300

    middleware

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,5 +1,6 @@
 import { randomUUID } from 'node:crypto';
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 // - Criar usuários
 // - Listagem usuários
@@ -23,21 +24,10 @@ const users = []
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-Type', 'application/json')
       .end(JSON.stringify(users))
   }
 
