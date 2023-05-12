dfce2c26b4c4e823dc24801407685b84c5963b64
germanolira <dolan.3@hotmail.com>
2023-05-12 12:00:54 -0300

    create project

---
+++ 02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/hello', () => {
+    return 'world'
+})
+
+app.listen({
+    port: 3000,
+}).then(() => {
+    console.log('HTTP Server Running')
+})
