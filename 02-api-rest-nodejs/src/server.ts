439bcd8426400e44e16c510891799936fdee1045
germanolira <dolan.3@hotmail.com>
2023-05-17 17:55:10 -0300

    create plugins add module typescript

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Test transaction',
-      amount: 100,
-    })
-    .returning('*')
-
-  return transactions
+app.register(transactionRoutes, {
+  prefix: 'transactions',
 })
 
 app
