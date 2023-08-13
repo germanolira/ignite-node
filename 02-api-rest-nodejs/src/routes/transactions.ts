a7f48b262c9729d53647e6a65b6858c4016de9be
Germano Lira <dolan.3@hotmail.com>
2023-08-13 02:52:58 -0300

    Update transactions.ts

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -10,7 +10,7 @@ export async function transactionRoutes(app: FastifyInstance) {
     {
       preHandler: [checkSessionIdExists],
     },
-    async (request, reply) => {
+    async (request) => {
       const { sessionId } = request.cookies
 
       const transactions = await knex('transactions')
